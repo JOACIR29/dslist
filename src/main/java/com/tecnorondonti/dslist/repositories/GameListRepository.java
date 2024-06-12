0a57a2b307cdb17b44d7d7da4211931225ed7860
JOACIR VIANA <joacir.ines2014@gmail.com>
2024-06-11 20:54:15 -0400

    GET Lists

---
+++ src/main/java/com/tecnorondonti/dslist/repositories/GameListRepository.java
@@ -0,0 +1,9 @@
+package com.tecnorondonti.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tecnorondonti.dslist.entities.GameList;
+
+public interface GameListRepository extends JpaRepository<GameList, Long>{
+
+}
